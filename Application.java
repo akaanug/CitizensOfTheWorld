b75ca6fa25f9ba49695268ca8fac82fef787225e
sacak32 <48670841+sacak32@users.noreply.github.com>
2019-03-26 20:38:50 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -2,45 +2,52 @@ import java.awt.*;        // Using AWT layouts
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import javax.swing.*;     // Using Swing components and containers
 import mainCode.*;
+import java.util.ArrayList;
 
 // A Swing GUI application inherits from top-level container javax.swing.JFrame
 public class Application extends JFrame {
  
    // Private instance variables
    MainMenu mainMenu;
-   Credits credits;
-   HowToPlay howToPlay;
-   LoadGame loadGame;
    PlayerMenu playerMenu;
+   LoadGame loadGame;
+   HowToPlay howToPlay;
+   Credits credits;
    GameGUI gameGui;
+   ArrayList<JPanel> panels;
    Game game;
-   
+
    // Constructor to setup the GUI components and event handlers
    public Application() 
    {
       setLayout( new BorderLayout() );
+           
+      panels = new ArrayList<JPanel>();
+      
+      mainMenu = new MainMenu( this );
+      panels.add( mainMenu );
       
       playerMenu = new PlayerMenu( this );
-      add( playerMenu );
-            
-      loadGame = new LoadGame( this );
-      add( loadGame );
+      panels.add( playerMenu );
       
-      credits = new Credits( this );
-      add( credits );
+      loadGame = new LoadGame( this );
+      panels.add( loadGame );
       
       howToPlay = new HowToPlay( this );
-      add( howToPlay );
-    
-      mainMenu = new MainMenu( this );
-      add( mainMenu );
+      panels.add( howToPlay );
+      
+      credits = new Credits( this );
+      panels.add( credits );
+      
+      for ( int n = 0; n < panels.size(); n++ )
+      {
+         add( panels.get( n ) );
+      }
       
       setTitle("Citizens Of The World");  // "super" JFrame sets title
-      setSize(700, 700);   // "super" JFrame sets initial size
+      setSize(1366, 768);   // "super" JFrame sets initial size
       setVisible(true);    // "super" JFrame shows
-      setLocation( 300, 25 );
-      
-      System.out.println( );
+      setLocation( 0, 0 );
    }
    
    // The entry main() method
