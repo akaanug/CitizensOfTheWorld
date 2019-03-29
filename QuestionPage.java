c7b0a4ddec84182a40b02797f7e00414af84503b
sacak32 <48670841+sacak32@users.noreply.github.com>
2019-03-30 00:20:59 +0300

    Add files via upload

--- QuestionPage.java
+++ QuestionPage.java
@@ -11,9 +11,13 @@ public class QuestionPage extends JPanel {
    // private variables
    public static final int MAX_QUESTION_NUMBER = 3;
    public static final int CHOICE_NUMBER = 3;
+   public static final int ONE_SECOND = 1000;
+   public int questionTime = 15;
    Questions questions;
    Question currentQuestion;
    JLabel header;
+   JLabel timeLabel;
+   Timer timer;
    JLabel questionSentence;
    JButton choiceOne;
    JButton choiceTwo;
@@ -36,6 +40,9 @@ public class QuestionPage extends JPanel {
       header = new JLabel();
       add( header );
       
+      timeLabel = new JLabel( questionTime + "" );
+      add( timeLabel );
+      
       questionSentence = new JLabel();
       add( questionSentence );
       
@@ -57,54 +64,87 @@ public class QuestionPage extends JPanel {
          add( choiceButtons.get( n ) );
          choiceButtons.get( n ).addActionListener( new ChoiceBtnListener() );
       }
+      
+      // Time Arranger
+      timer = new Timer(ONE_SECOND, new QuestionTimeListener() );
+      timer.start();
    }
    
-   public QuestionPage( Questions questions, int countryIncome, GameGUI parent ) 
-   {
-      setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
-  
-      // Declare the variables and add them to the panel
-      this.questions = questions;
-      questionNumber = 0;
-      currentQuestion = questions.getQuestion( questionNumber );
-      this.countryIncome = countryIncome;
-      
-      header = new JLabel( "QUESTION   " + ( questionNumber + 1 ) );
-      add( header );
-      
-      questionSentence = new JLabel( currentQuestion.getQuestionSentence() );
-      add( questionSentence );
-      
-         // Declare Buttons
-      choiceButtons = new ArrayList<JButton>();
-      
-      choiceOne = new JButton( currentQuestion.getChoice( 0 ) );
-      choiceButtons.add( choiceOne );
-      
-      choiceTwo = new JButton( currentQuestion.getChoice( 1 ) );
-      choiceButtons.add( choiceTwo );
-      
-      choiceThree = new JButton( currentQuestion.getChoice( 2 ) );
-      choiceButtons.add( choiceThree );
-      
-         // Add Buttons to the Panel while arranging their event handlers
-      for ( n = 0; n < CHOICE_NUMBER; n++ )
-      {
-         add( choiceButtons.get( n ) );
-         choiceButtons.get( n ).addActionListener( new ChoiceBtnListener() );
-      }
-      
-      setVisible( true );
-   }
+//   public QuestionPage( Questions questions, int countryIncome, GameGUI parent ) 
+//   {
+//      setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
+//  
+//      // Declare the variables and add them to the panel
+//      this.questions = questions;
+//      questionNumber = 0;
+//      currentQuestion = questions.getQuestion( questionNumber );
+//      this.countryIncome = countryIncome;
+//      
+//      header = new JLabel( "QUESTION   " + ( questionNumber + 1 ) );
+//      add( header );
+//      
+//      timeLabel = new JLabel( questionTime + "" );
+//      add( timeLabel );
+//      
+//      questionSentence = new JLabel( currentQuestion.getQuestionSentence() );
+//      add( questionSentence );
+//      
+//         // Declare Buttons
+//      choiceButtons = new ArrayList<JButton>();
+//      
+//      choiceOne = new JButton( currentQuestion.getChoice( 0 ) );
+//      choiceButtons.add( choiceOne );
+//      
+//      choiceTwo = new JButton( currentQuestion.getChoice( 1 ) );
+//      choiceButtons.add( choiceTwo );
+//      
+//      choiceThree = new JButton( currentQuestion.getChoice( 2 ) );
+//      choiceButtons.add( choiceThree );
+//      
+//         // Add Buttons to the Panel while arranging their event handlers
+//      for ( n = 0; n < CHOICE_NUMBER; n++ )
+//      {
+//         add( choiceButtons.get( n ) );
+//         choiceButtons.get( n ).addActionListener( new ChoiceBtnListener() );
+//      }
+//      
+//      // Time Arranger
+//      timer = new Timer(ONE_SECOND, new QuestionTimeListener() );
+//
+//      setVisible( true );
+//      timer.start();
+//   }
 
    // Methods  
+   
+   // Question Time Listener
+   public class QuestionTimeListener implements ActionListener
+   {
+      @Override
+      public void actionPerformed( ActionEvent evt )
+      {
+         questionTime--;
+         
+         timeLabel.setText( questionTime + "" );
+         
+         if ( questionTime == 0 ) 
+         {
+            timer.stop();
+            parent.simplePages.youLose( );
+            setVisible( false );
+            questionTime = 15;
+         }
+      }
+   }
    
    // Choice Button Listener
    public class ChoiceBtnListener implements ActionListener
    {
       @Override
       public void actionPerformed( ActionEvent evt )
-      {
+      {                  
+         timer.stop();
+         
          if ( choiceButtons.indexOf( evt.getSource() ) == currentQuestion.getAnswer() )
          {
             if ( questionNumber < MAX_QUESTION_NUMBER - 1 )
@@ -122,7 +162,6 @@ public class QuestionPage extends JPanel {
             parent.simplePages.youLose( );
             setVisible( false );
          }
-         
       }
    }
    
@@ -130,13 +169,17 @@ public class QuestionPage extends JPanel {
    {
       this.questionNumber = questionNumber;
       currentQuestion = questions.getQuestion( questionNumber );
-      
+      questionTime = 15;
+              
+      timeLabel.setText( questionTime + "" );      
       header.setText( "QUESTION   " + ( questionNumber + 1 ) );
       questionSentence.setText( currentQuestion.getQuestionSentence() );
       for ( n = 0; n < CHOICE_NUMBER; n++ )
       {
          choiceButtons.get( n ).setText( currentQuestion.getChoice( n ) );
       }
+      
+      timer.start();
    }
    
    public void totallyNewQuestions( Questions questions, int countryIncome )
@@ -146,8 +189,10 @@ public class QuestionPage extends JPanel {
       questionNumber = 0;
       currentQuestion = questions.getQuestion( questionNumber );
       this.countryIncome = countryIncome;
+      questionTime = 15;
       
       // Arranging some panels and buttons 
+      timeLabel.setText( questionTime + "" ); 
       header.setText( "QUESTION   1" ); 
       questionSentence.setText( currentQuestion.getQuestionSentence() );
       for ( n = 0; n < CHOICE_NUMBER; n++ )
@@ -156,6 +201,7 @@ public class QuestionPage extends JPanel {
       }
       
       setVisible( true );
+      timer.start();
    }
 }   
          
