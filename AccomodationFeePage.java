846992da60be5808b470be71cb6f30d113863f51
akaanug <46591206+akaanug@users.noreply.github.com>
2019-04-08 11:07:48 +0300

    Accomodation paneladded

--- AccomodationFeePage.java
+++ AccomodationFeePage.java
@@ -6,28 +6,33 @@ import mainCode.*;
 // A GUI program is written as a subclass of Frame - the top-level container
 // This subclass inherits all properties from Frame, e.g., title, icon, buttons, content-pane
 public class AccomodationFeePage extends JPanel {
-
+   
    // private variables
-   JLabel header;
    JLabel money;
+   JLabel all;
    JButton exit;
-   GameGUI p;
+   private ImageIcon image;
+   GameGUI parent;
    
    // Constructor to setup the GUI components
-   public AccomodationFeePage( GameGUI parent ) 
+   public AccomodationFeePage( GameGUI p ) 
    {
-      setLayout( new BorderLayout() );
+      all = new JLabel();
       
-      this.p = parent;
+      all.setLayout(null);
       
-      header = new JLabel( "Accomodation Fee: " );
-      add( header, BorderLayout.NORTH );
+      //this.parent = p;
+      all.setPreferredSize( new Dimension(  481, 567 ) );
+      money = new JLabel( "money" );
       
-      money = new JLabel( );
-      add( money );
+      money.setBounds( 152, 439, 184, 75 );
+      money.setFont( new Font("Arial", Font.BOLD, 48) );
+      money.setOpaque(true);
+      money.setBackground(Color.white);
+      all.add( money );
       
       exit = new JButton( "Exit" );
-      add( exit, BorderLayout.SOUTH );
+      exit.setBounds( 0, 0, 450, 30 );
       exit.addActionListener( new ActionListener() { 
          @Override
          public void actionPerformed( ActionEvent evt )
@@ -37,9 +42,11 @@ public class AccomodationFeePage extends JPanel {
          }
       } );
       
+      all.add(exit);
+      add(all);
       setVisible( false );
    }
-
+   
    // Methods  
    public void refresh( int accomodationFee )
    {
@@ -47,4 +54,23 @@ public class AccomodationFeePage extends JPanel {
       
       setVisible( true );
    }
+   
+   public void paintComponent(Graphics page)
+   {
+      super.paintComponent (page);
+      
+      image = new ImageIcon ("Background Photos\\AccomodationFrame.gif");
+      image.paintIcon (this, page, 0, 0);
+   }
+   
+   //test için setVisible true yapmayı unutma
+//      public static void main (String[] args)
+//   {
+//      JFrame f = new JFrame();
+//      AccomodationFeePage p = new AccomodationFeePage();
+//      f.add(p);
+//      f.setResizable(false);
+//      f.setSize( 481, 567 );
+//      f.setVisible( true );
+//   }
 }
