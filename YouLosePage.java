e0a2d3a0a4f5ca083bfbde41a81e9bab41379df1
akaanug <46591206+akaanug@users.noreply.github.com>
2019-04-08 10:40:44 +0300

    Update YouLosePage.java

--- YouLosePage.java
+++ YouLosePage.java
@@ -6,28 +6,36 @@ import mainCode.*;
 // A GUI program is written as a subclass of Frame - the top-level container
 // This subclass inherits all properties from Frame, e.g., title, icon, buttons, content-pane
 public class YouLosePage extends JPanel {
-
+   
    // private variables
-   JLabel header;
    JLabel money;
+   JLabel all;
    JButton exit;
+   private ImageIcon image;
    GameGUI parent;
    
    // Constructor to setup the GUI components
    public YouLosePage( GameGUI p ) 
    {
-      setLayout( new BorderLayout() );
+      //setBorderLayout();
+      
+      
+      all = new JLabel();
+      
+      all.setLayout(null);
       
       this.parent = p;
+      all.setPreferredSize( new Dimension(  481, 567 ) );
+      money = new JLabel( "money" );
       
-      header = new JLabel( "You Lose" );
-      add( header, BorderLayout.NORTH );
-      
-      money = new JLabel( );
-      add( money );
+      money.setBounds( 138, 370, 198, 78 );
+      money.setFont( new Font("Arial", Font.BOLD, 48) );
+      money.setOpaque(true);
+      money.setBackground(Color.white);
+      all.add( money );
       
       exit = new JButton( "Exit" );
-      add( exit, BorderLayout.SOUTH );
+      exit.setBounds( 0, 0, 450, 30 );
       exit.addActionListener( new ActionListener() { 
          @Override
          public void actionPerformed( ActionEvent evt )
@@ -37,14 +45,27 @@ public class YouLosePage extends JPanel {
          }
       } );
       
+      all.add(exit);
+      
+      add(all);
+      
       setVisible( false );
    }
-
-   // Methods  
+   
+   //Methods  
    public void refresh( int questionNumber  )
    {
       money.setText( "- " + ( 10 * ( questionNumber + 1 ) ) );
       
       setVisible( true );
    }
+   
+   public void paintComponent(Graphics page)
+   {
+      super.paintComponent (page);
+         
+      image = new ImageIcon ("Background Photos\\LoseFrame.gif");
+      image.paintIcon (this, page, 0, 0);
+   }
+   
 }
