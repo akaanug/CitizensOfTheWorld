e920ba5fdbd179d5a58d449a1f2298d3b0394411
sacak32 <48670841+sacak32@users.noreply.github.com>
2019-04-13 19:03:21 +0300

    Add files via upload

---
+++ gui/LoadGame.java
@@ -0,0 +1,41 @@
+package gui;
+
+import java.awt.*;  
+import java.awt.event.*;
+import javax.swing.*;
+
+// A GUI program is written as a subclass of Frame - the top-level container
+// This subclass inherits all properties from Frame, e.g., title, icon, buttons, content-pane
+public class LoadGame extends JPanel {
+
+   // private variables
+   JButton back;
+   Application app;
+   
+   // Constructor to setup the GUI components
+   public LoadGame( Application app ) 
+   {      
+      setLayout( new BorderLayout() );
+      setVisible( false );
+      setSize( app.getSize() );
+      
+      this.app = app;
+      
+      back = new JButton( "Back" );
+      add( back, BorderLayout.SOUTH ); 
+      back.addActionListener( new BackBtnListener() );
+   }
+   
+   // methods
+   
+   // Back Button Listener
+   public class BackBtnListener implements ActionListener
+   {
+      @Override
+      public void actionPerformed( ActionEvent evt )
+      {
+         setVisible( false );
+         app.mainMenu.setVisible( true );
+      }
+   }
+}
---
+++ mainCode/Quiz.java
@@ -0,0 +1,88 @@
+package mainCode;
+
+import java.util.Observable;
+
+public class Quiz extends Observable
+{
+   // properties
+   public static final int QUESTION_NUMBER = 3;
+   Questions quizQuestions;
+   int questionNumber;
+   boolean quizzing;
+   
+   // constructors
+   public Quiz()
+   {
+      quizzing = false;
+   }
+   
+   public Quiz( Country c )
+   {
+      newQuiz( c );
+   }
+   
+   // methods
+   public int getQuestionNumber()
+   {
+      return questionNumber;
+   }
+   
+   public boolean isQuizzing()
+   {
+      return quizzing;
+   }
+   
+   public void quizEnded()
+   {
+      quizzing = false;
+      notifier();
+   }
+   
+   public void newQuiz( Country c )
+   {
+      quizQuestions = c.determineThreeRandomQuestions();
+      questionNumber = 0;
+      quizzing = true;
+      
+      notifier();
+   }
+   
+   public void nextQuestion()
+   {
+      questionNumber++;
+      notifier();
+   }
+   
+   public Question getQuestion()
+   {
+      return quizQuestions.get( questionNumber );
+   }
+   
+   public void notifier()
+   {
+      setChanged();
+      notifyObservers();
+   }
+   
+   public int checkAnswer( int answer )
+   {
+      if ( getQuestion().isAnswerCorrect( answer ) )
+      {
+         if ( questionNumber < QUESTION_NUMBER - 1 )
+         {
+            nextQuestion();
+            return 1;
+         }
+         else 
+         {     
+            quizEnded();
+            return 2;
+         }
+      }
+      else
+      {
+         quizEnded();
+         return 0;
+      }     
+   }
+}
