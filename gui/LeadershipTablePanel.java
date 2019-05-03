707135d1c7f495c79fb53e82f69cd8026125e943
akaanug <46591206+akaanug@users.noreply.github.com>
2019-05-03 12:15:14 +0300

    Changed LeadershipTablePanel & Added LT class for that

--- gui/LeadershipTablePanel.java
+++ gui/LeadershipTablePanel.java
@@ -10,55 +10,54 @@ import java.util.Observable;
 
 // A Swing GUI application inherits from top-level container javax.swing.JFrame
 public class LeadershipTablePanel extends JPanel implements Observer {
- 
-   // Private instance variables
-   JPanel[] playerInfos;
-   LeadershipTable leadershipTable;
-   
-   // Constructor to setup the GUI components and event handlers
-   public LeadershipTablePanel( LeadershipTable leadershipTable ) 
-   {
-      setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );        
-     
-      this.leadershipTable = leadershipTable;
-      leadershipTable.addObserver( this );
-      
-      createComponents();
-      
-      setSize(250, 700);   
-      setVisible( false );   
-   }   
-   
-   // methods
-   
-   public void createComponents()
-   {
-      add( new Label( "Leadership Table" ) );
-          
-      playerInfos = new JPanel[ leadershipTable.size() ];
-      for( int n = 0; n < playerInfos.length; n++ )
-      {
-          playerInfos[n] = new JPanel();
-      }
-      
-      for( int n = 0; n < playerInfos.length; n++ )
-      {
-         playerInfos[n].add( new JLabel( ) );
-         playerInfos[n].add( new JLabel( ) );
-         playerInfos[n].add( new JLabel( ) );
-         playerInfos[n].add( new JLabel( ) );
-         add( playerInfos[n] );
-      }
-   }
-   
-   public void update( Observable obs, Object obj )
-   {      
-      for( int n = 0; n < playerInfos.length; n++ )
-      {
-         ( (JLabel) playerInfos[n].getComponent( 0 ) ).setText( ( n + 1 ) + "     " );
-         ( (JLabel) playerInfos[n].getComponent( 1 ) ).setText( leadershipTable.get( n ).getName() + "     " );
-         ( (JLabel) playerInfos[n].getComponent( 2 ) ).setText( leadershipTable.get( n ).getNumberOfCountries() + "     " );
-         ( (JLabel) playerInfos[n].getComponent( 3 ) ).setText( leadershipTable.get( n ).getMoney() + "" );
-      }
-   }    
+    
+    // Private instance variables
+    LT[] playerInfos;
+    LeadershipTable leadershipTable;
+    JLabel lt;
+    
+    // Constructor to setup the GUI components and event handlers
+    public LeadershipTablePanel( LeadershipTable leadershipTable ) 
+    {
+        setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );        
+        
+        this.leadershipTable = leadershipTable;
+        leadershipTable.addObserver( this );
+        
+        createComponents();
+        
+        setSize(250, 700); 
+        setBackground(new java.awt.Color(102, 255, 255));
+        setVisible( false );   
+    }   
+    
+    // methods
+    
+    public void createComponents()
+    {
+        lt = new JLabel();
+        lt.setBackground(new java.awt.Color(102, 255, 255));
+        lt.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        lt.setForeground(new java.awt.Color(255, 0, 0));
+        lt.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lt.setText("LEADERSHIP TABLE");
+        lt.setOpaque(true);
+        add(lt);
+        
+        playerInfos = new LT[ leadershipTable.size() ];
+        
+        for( int n = 0; n < leadershipTable.size(); n++ )
+        {
+            playerInfos[n] = new LT( n + 1, leadershipTable.get(n).getName(), leadershipTable.get( n ).getNumberOfCountries(), leadershipTable.get( n ).getMoney() );
+            add( playerInfos[n] );
+        }
+    }
+    
+    public void update( Observable obs, Object obj )
+    {
+        for( int n = 0; n < playerInfos.length; n++ )
+        {
+            playerInfos[n].change( n + 1, leadershipTable.get(n).getName(), leadershipTable.get( n ).getNumberOfCountries(), leadershipTable.get( n ).getMoney() );
+        }
+    }
 }
